7543d56f3c1aec1b3608e48a9f2e9ce5727e6223
MykhailoYehorov <egorovmihaili4@gmail.com>
2022-12-07 20:53:24 +0200

    Initial commit

---
+++ src/Product.java
@@ -0,0 +1,51 @@
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    //constructor
+
+
+    public Product() {
+    }
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+//setters
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
---
+++ src/ProductNext.java
@@ -0,0 +1,27 @@
+public class ProductNext {
+    public static void main(String[] args) {
+        Product p1 = new Product();
+        p1.getName();
+        p1.getPrice();
+        p1.getQuantity();
+        System.out.println(p1);
+
+        Product p2 = new Product();
+        p2.getName();
+        p2.getPrice();
+        p2.getQuantity();
+        System.out.println(p2);
+
+        Product p3 = new Product();
+        p3.getName();
+        p3.getPrice();
+        p3.getQuantity();
+        System.out.println(p3);
+
+        Product p4 = new Product();
+        p4.getName();
+        p4.getPrice();
+        p4.getQuantity();
+        System.out.println(p4);
+    }
+}
